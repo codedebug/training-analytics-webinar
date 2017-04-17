beccc2fce577695a612e8c8562b0fd43622e58ed
Daniela Matos de Carvalho <alunassertiva@gmail.com>
2017-04-17 22:59:44 +0100

    remove old comment

--- packages/publisher/test/publisher.js
+++ packages/publisher/test/publisher.js
@@ -4,45 +4,13 @@ const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 const describe = lab.experiment;
 const before = lab.before;
-// const after = lab.after;
 const it = lab.it;
 const Code = require('code');
 const expect = Code.expect;
 
 const publisher = require('../src/publisher');
-// const sinon = require('sinon');
-// const amqp = require('amqplib/callback_api');
 
 describe('publisher', () => {
-  // TOFIX: only works with it.only (issue with publisher.getInstance)
-  // describe('with a invalid connection', () => {
-  //   let sandbox;
-  //   let connectStub;
-  //
-  //   before((done) => {
-  //     sandbox = sinon.sandbox.create();
-  //     connectStub = sandbox.stub(amqp, 'connect').callsFake((options, callback) => {
-  //       return callback(new Error('Err'));
-  //     });
-  //     done();
-  //   });
-  //
-  //   after((done) => {
-  //     sandbox.restore();
-  //     done();
-  //   });
-  //
-  //   it('should throw an error when amqp.connect() is called', (done) => {
-  //     publisher.getInstance((err, result) => {
-  //       expect(connectStub.called).to.be.true();
-  //       expect(err).to.exist();
-  //       expect(err).to.be.an.object();
-  //       expect(err.message).to.be.equal('Err');
-  //       done();
-  //     });
-  //   });
-  // });
-
   describe('with a valid connection', () => {
     let pub;
 
